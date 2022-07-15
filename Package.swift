78d9142786836d92a2f0512b6db0d5c3ec0dcf74
John DeLong <jdelong@michiganlabs.com>
2022-07-14 22:32:09 -0400

    Bump version to 7.0.0

--- Package.swift
+++ Package.swift
@@ -41,28 +41,28 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GoogleMaps",
-            url: "https://github.com/MichiganLabs/GoogleMaps-SP/releases/download/6.2.1-universal/GoogleMaps.xcframework.zip",
-            checksum: "94b529f0320ac59e14009bf578b7855d3c437ce6de0e2619502afd3c50301620"
+            url: "https://github.com/MichiganLabs/GoogleMaps-SP/releases/download/7.0.0-universal/GoogleMaps.xcframework.zip",
+            checksum: "05b5f7cef5c3c2f204e021304f1507520f50aa0851582a13ee0b7deeea2c819f"
         ),
         .binaryTarget(
             name: "GoogleMapsBase",
-            url: "https://github.com/MichiganLabs/GoogleMaps-SP/releases/download/6.2.1-universal/GoogleMapsBase.xcframework.zip",
-            checksum: "a3cec21a5d2e5d38d5ff0a2dc5b901113f330f36f0d19320489ccabbb318b47e"
+            url: "https://github.com/MichiganLabs/GoogleMaps-SP/releases/download/7.0.0-universal/GoogleMapsBase.xcframework.zip",
+            checksum: "7c8c931a21f433b78a249b189cb877544acc586ef3e25da04642c535dfba31eb"
         ),
         .binaryTarget(
             name: "GoogleMapsCore",
-            url: "https://github.com/MichiganLabs/GoogleMaps-SP/releases/download/6.2.1-universal/GoogleMapsCore.xcframework.zip",
-            checksum: "58758af0e1b2085a49bd307e03b84313924ed460c63f4d5fe04d8a366393be34"
+            url: "https://github.com/MichiganLabs/GoogleMaps-SP/releases/download/7.0.0-universal/GoogleMapsCore.xcframework.zip",
+            checksum: "14a9b5c44865945a20094f4082565727d4822dc8fb418067fc55dbef0ac25ed2"
         ),
         .binaryTarget(
             name: "GoogleMapsM4B",
-            url: "https://github.com/MichiganLabs/GoogleMaps-SP/releases/download/6.2.1-universal/GoogleMapsM4B.xcframework.zip",
-            checksum: "67c474ce836d2b0bbc33af396566ab09fa60ff4d50672d0f07f606b5f7bfbd8b"
+            url: "https://github.com/MichiganLabs/GoogleMaps-SP/releases/download/7.0.0-universal/GoogleMapsM4B.xcframework.zip",
+            checksum: "275627d38f8a26f1554d6fc68b800f4449f7d23b6fbeab0c7beed611094c7c9b"
         ),
         .binaryTarget(
             name: "GooglePlaces",
-            url: "https://github.com/MichiganLabs/GoogleMaps-SP/releases/download/6.2.1-universal/GooglePlaces.xcframework.zip",
-            checksum: "71ca0c78e26b647a3d3b883ed9891152b71373b2aa71f62b7897de56497684f3"
+            url: "https://github.com/MichiganLabs/GoogleMaps-SP/releases/download/7.0.0-universal/GooglePlaces.xcframework.zip",
+            checksum: "3ceef0d4c3297caea93d2f7ed062cfe5e57e50678f86192ab5f21bb3dc2c5838"
         )
     ]
 )
